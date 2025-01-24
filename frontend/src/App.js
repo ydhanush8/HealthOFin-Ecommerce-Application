64bbc13669a422bcd3db406d4f7bd90dcc32a351
Dhanush <dhanushsaireddy8@gmail.com>
2025-01-24 17:39:42 +0530

    Added Sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Products from './Products';
 import Cart from './Cart';
-import './App.css'
+import './App.css';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Router>
       <div className="App">
         <nav className="navbar">
-          <ul>
-            <div className='logo'>Ecommerce</div>
-            <li>
-              <Link to="/" className='nav-link'>Products</Link>
-            </li>
-            <li>
-              <Link to="/cart" className='nav-link'>Cart</Link>
-            </li>
-          </ul>
+          <div className="hamburger" onClick={toggleSidebar}>
+            ☰
+          </div>
+          <div className="logo">QuickCart Online</div>
         </nav>
+
+        {isSidebarOpen && (
+          <>
+            <div className="overlay" onClick={toggleSidebar}></div>
+            <div className="sidebar">
+              <button className="close-btn" onClick={toggleSidebar}>
+                ✕
+              </button>
+              <ul>
+                <li className='nav-link-li nav-link-top'>
+                  <Link to="/" className="nav-link" onClick={toggleSidebar}>
+                    Products
+                  </Link>
+                </li>
+                <li className='nav-link-li'>
+                  <Link to="/cart" className="nav-link" onClick={toggleSidebar}>
+                    Cart
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
         <Routes>
-          <Route path="/" element={<Products onAddToCart />} />
-          <Route path="/cart" element={<Cart cartItems />} />
+            <Route path="/" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
     </Router>
